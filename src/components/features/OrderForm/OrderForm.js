35ce81c504cdb2c0796b47b29d0daf553ee7383b
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2021-01-11 16:20:40 +0100

    Add showing options' titles

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { Row, Col } from 'react-flexbox-grid';
 
+import pricing from '../../../data/pricing.json';
+
 import OrderSummary from '../OrderSummary/OrderSummary';
+import OrderOption from '../OrderOption/OrderOption';
 
 const OrderForm = ({ tripCost, options }) => (
   <Row>
+    {pricing.map(pricingOption => (
+      <Col md={4} key={pricingOption.id} >
+        <OrderOption {...pricingOption} />
+      </Col>
+    ))}
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options} />
     </Col>
