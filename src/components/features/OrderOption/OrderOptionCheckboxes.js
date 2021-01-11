443db46ae5e1962351eb1c5cb9ec4c3e722157f3
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2021-01-11 16:49:42 +0100

    Add showing options' types names and correct location of OrderOption components

---
+++ src/components/features/OrderOption/OrderOptionCheckboxes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const OrderOptionCheckboxes = () => {
+  return (
+    <div>OrderOptionCheckboxes</div>
+  );
+};
+
+export default OrderOptionCheckboxes;
---
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const OrderOptionIcons = () => {
+  return (
+    <div>OrderOptionIcons</div>
+  );
+};
+
+export default OrderOptionIcons;
